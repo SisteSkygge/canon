63e88f11f0a964a6180822a2fe962e97fbc946a1
SisteSkygge <lordigna@gmail.com>
2018-03-04 13:40:55 +0100

    Add files via upload

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
 
+import math
+import time
 import socket
 import sys
 import pickle
@@ -19,6 +21,7 @@ class server_listener(Thread):
         Thread.__init__(self)
         self.socket = connect_socket
         self.active = 1
+        self.msg = ""
 
     def run(self):
         while self.active:
@@ -70,15 +73,41 @@ class Game(Thread):
         self.port_server.pack()
 
     def ecran_selection_serveur(self):
-        #Affiche l'ecran de selection de la partie
+        #Le time.sleep est la pour que que le thread listener puisse recevoir le message en premier et
+        #qu'ensuite seulement le thread game puisse lire le message reçu
+        while(self.listener.get_msg().split(" ")[0] != "IG"):
+            time.sleep(0.25)
         if(self.listener.get_msg()!=""):
-            #Dans le cas ou cest pas vide on affiche les parties
-            pass
+            self.afficher_serveur(self.listener.get_msg())
         #On affiche les boutons de bases
         self.button_create_party = Button(self.root, text="Créer une partie", command=self.create_party)
         self.button_quit_server = Button(self.root, text="Quitter le serveur", command=self.quit_server)
         self.button_create_party.pack()
         self.button_quit_server.pack()
+
+    def ecran_lobby(self):
+        #Intruction pour l'affichage de l'ecran
+        self.clean_screen()
+        lobby_label = Label(self.root, text="Yeah lobby !")
+        lobby_label.pack()
+        while(self.listener.get_msg().split(" ")[0] != "GD"):
+            time.sleep(0.25)
+        self.ecran_jeu()
+
+    def ecran_jeu(self):
+        #Attente du premier message relatif au jeu les suivants seront lu au fil du jeu et non en l'interromptant
+        while(self.listener.get_msg().split(" ")[0] != "GD"):
+            time.sleep(0.25)
+        self.clean_screen()
+        self.canvas = Canvas(self.root, width=450, height=350)
+        self.canvas.pack()
+        server_quit_button = Button(self.root, text="Quitter le serveur", command=self.quit_server)
+        server_quit_button.pack()
+        self.mCanon = Canon(self.canvas, 20, 250)
+        self.mBoulet = Boulet(self.canvas, 20, 175, 0, 0)
+        self.mSol = Sol(self.canvas, 0, 250, 450, 250)
+        self.mCanon.draw()
+        self.root.after(0, self.animation)
 
     def get_adress(self):
         return self.adress_server.get()
@@ -87,14 +116,61 @@ class Game(Thread):
     def clean_screen(self):
         for element in self.root.winfo_children():
             element.destroy()
+    def afficher_serveur(self, message):
+        #split " " pour séparé les parties
+        #split : pour le numero de la partie
+        #split , pour le nombre de joueur
+        #le reste est le statut de la game
+        print(message)
+        game = message.split(" ")
+        self.button_server = []
+        print(game)
+        for element in game:
+            if(len(game)==2):
+                break
+            elif(element==""):
+                pass
+            elif(element=="IG"):
+                pass
+            else:
+                print(element)
+                buffer_game = element.split(":")
+                id_game = buffer_game[0]
+                buffer_game = buffer_game[1].split(",")
+                nb_joueur = buffer_game[0]
+                statut = buffer_game[1]
+                self.button_server.append(Button(self.root, text="Id game : {}\nNombre de joueurs : {}\nStatut : {}"\
+                    .format(id_game, nb_joueur, statut)\
+                    , command=lambda id_button=id_game: self.join_game(id_button)))
+        for element in self.button_server:
+            element.pack()
 
     def create_party(self):
         self.messager.send_msg("New party")
+        #TODO suite d'instruction pour charger le lobby d'attente de la partie
+        self.ecran_lobby()
 
     def quit_server(self):
         self.socket.close()
         self.clean_screen()
         self.ecran_principal()
+
+    def join_game(self, id_button):
+        self.messager.send_msg("Join {}".format(id_button))
+        #TODO suite d'instruction pour charger le lobby d'attente de la partie
+        active = True
+        while active:
+            time.sleep(0.25)
+            if(self.listener.get_msg().split(' ')[0]=="IC"):
+                if("Full" in self.listener.get_msg()):
+                    Label(self.root, text="Serveur complet")
+                else:
+                    active = False
+            if(self.listener.get_msg().split(" ")[0]=="IJ"):
+                active = False
+
+        self.ecran_lobby()
+
 
     def jouer(self):
         #try:
@@ -115,6 +191,13 @@ class Game(Thread):
         #   print("Connexion interrompu ou indisponible")
         #   self.ecran_principal()
 
+    def animation(self):
+        self.canvas.create_rectangle(0,0,450,350, fill="white")
+        self.mCanon.update(self.root.winfo_pointerx()-self.root.winfo_rootx(), self.root.winfo_pointery()-self.root.winfo_rooty())
+        self.mBoulet.draw()
+        self.mSol.draw()
+        self.root.after(500, self.animation)
+
     def co_test(self):
         try:
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -127,6 +210,43 @@ class Game(Thread):
         except:
             self.status = Label(self.root, text="La connexion ne peut pas etre etabli")
             self.status.pack()
+
+class Canon:
+    def __init__(self, canvas, x, y):
+        self.canvas = canvas
+        self.x = x
+        self.y = y
+        self.height = 30
+        self.width = 15
+
+    def draw(self):
+        self.canvas.create_rectangle(self.x-self.width/2, self.y, self.x+self.width/2, self.y-self.height)
+    def update(self, mouse_x, mouse_y):
+        print(str(mouse_x)+" "+str(mouse_y))
+        self.canvas.create_rectangle(self.x-self.width/2, self.y, self.x+self.width/2+math.cos(self.x-mouse_x), self.y-self.height+math.sin(self.y-mouse_y))
+
+class Boulet:
+    def __init__(self, canvas, x ,y, vx, vy):
+        self.canvas = canvas
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.rayon = 10
+
+    def draw(self):
+        self.canvas.create_oval(self.x-self.rayon/2, self.y-self.rayon/2, self.x+self.rayon/2, self.y+self.rayon/2, fill="black")
+
+class Sol:
+    def __init__(self, canvas, x1, y1, x2, y2):
+        self.canvas = canvas
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+
+    def draw(self):
+        self.canvas.create_line(self.x1, self.y1, self.x2, self.y2, fill="black")
 
 root = Tk()
 game = Game(root)
--- server.py
+++ server.py
@@ -4,6 +4,7 @@
 import socket
 from threading import Thread
 import pickle
+import time
 
 class Waiter(Thread):
     def __init__(self, socket, client_list, adress_list, listener_list, game_list):
@@ -49,22 +50,36 @@ class Listener(Thread):
                 self.msg = pickle.loads(self.msg)
                 print("{} : {}".format(self.adress, self.msg))
                 if(self.msg=="Test"):
+                    #Un joueur test si le serveur est online, on lui renvoie une réponse
                     self.client.send(pickle.dumps("Ok"))
                 if(self.msg=="Info session"):
+                    #Le joueur vient de se connecter on lui envoie les informations des parties existantes
                     print("Envoie des informations...")
                     self.give_info_session()
                 if(self.msg=="New party"):
+                    #Le joueur veut créer une partie
                     print("Création d'une nouvelle partie...")
                     self.create_new_game(self.adress)
+                if("Join" in self.msg):
+                    #Le joueur veut rejoindre une partie
+                    id_game = self.msg.split(" ")[1]
+                    if(self.game_list[int(id_game)].nb_joueur<2):
+                        #Il y a de la place
+                        self.client.send(pickle.dumps("IC Game:{}_join".format(id_game)))
+                        self.game_list[int(id_game)].ajouter_joueur(self.adress, self)
+                    else:
+                        #Il y a plus de place
+                        self.client.send(pickle.dumps("IC Full"))
                     
             except:
                 #On ferme la connexion
                 print("Deconnexion de {}".format(self.adress))
                 active = False
+                self.deco_client_from_game()
                 self.waiter.client_disconnected(self.client)
 
     def give_info_session(self):
-        data = ""
+        data = "IG "
         for i in range(len(self.game_list)):
             data += "{}:{},{} ".format(i, self.game_list[i].nb_joueur,\
                 self.game_list[i].status)
@@ -72,29 +87,153 @@ class Listener(Thread):
 
     def create_new_game(self, adress):
         self.game_list.append(Game())
-        self.game_list[len(self.game_list)-1].ajouter_joueur(self.adress)
-        data = "Game:{}_join".format(len(self.game_list)-1)
+        self.game_list[len(self.game_list)-1].start()
+        self.game_list[len(self.game_list)-1].ajouter_joueur(self.adress, self)
+        data = "IC Game:{}_join".format(len(self.game_list)-1)
         self.client.send(pickle.dumps(data))
+
+    def deco_client_from_game(self):
+        for element in self.game_list:
+            for i in range(len(element.joueur)):
+                if(self.adress==element.joueur[i]):
+                    del element.joueur[i]
                 
 
 class Game(Thread):
     def __init__(self):
         Thread.__init__(self)
         self.joueur = []
+        self.joueur_listener = []
         self.status = "En_attente"
-        self.nb_joueur = 0
+        self.nb_joueur = 1
 
     def run(self):
+        self.lobby()
+
+    def lobby(self):
         while(self.nb_joueur<2):
+            time.sleep(0.25)
             self.status = "En_attente"
             self.nb_joueur = len(self.joueur)
-        #Nombre de joueurs requis atteint
+            self.send_info_lobby_joueur()
+        compteur = 0
+        while(self.nb_joueur==2 and compteur<20):
+            time.sleep(0.25)
+            self.send_info_lobby_joueur()
+            self.status = "Complet"
+            self.nb_joueur = len(self.joueur)
+            compteur += 1
 
     def jouer(self):
-        pass
+        canon = []
+        sol = []
+        boulet = []
+        canon.append(Joueur(0, 20, 250))
+        canon.append(Joueur(1, 430, 250))
+        sol.append(Sol(0, 250, 450, 250))
+        while(self.nb_joueur==2):
+            self.nb_joueur = len(self.joueur)    
+            self.send_info_jeu(canon, boulet, sol)
+        #On affiche qui a gagne on fait un petit compteur puis on supprime la partie en supprimant les deux joueurs
+        #Comme le nombre de joueurs sera à 0 le game_cleaner supprimera la game
+        del self.joueur[0]
+        del self.joueur[1]
+        self.nb_joueur = len(self.joueur)
 
-    def ajouter_joueur(self, adress):
+    def ajouter_joueur(self, adress, joueur_listener):
         self.joueur.append(adress)
+        self.joueur_listener.append(joueur_listener)
+
+    def send_info_lobby_joueur(self):
+        data = "IJ "
+        for element in self.joueur:
+            data += str(element)+" "
+        if(self.nb_joueur==2):
+            data += "GO"
+        for element in self.joueur_listener:
+            element.client.send(pickle.dumps(data))
+
+    def send_info_jeu(self, canon, boulet, sol):
+        #Envoie les informations necessaire à tout les joueurs
+        data = "GD "
+        for element in canon:
+            data += element.return_info()
+        for element in boulet:
+            data += element.return_info()
+        for element in sol:
+            data += element.return_info()
+
+        for element in self.joueur_listener:
+            element.client.send(pickle.dumps(data))
+
+class Game_Cleaner(Thread):
+    def __init__(self, game_list):
+        Thread.__init__(self)
+        self.game_list = game_list
+
+    def run(self):
+        while True:
+            time.sleep(0.25)
+            for i in range(len(self.game_list)):
+                if(self.game_list[i].nb_joueur==0):
+                    del self.game_list[i]
+
+class Joueur:
+    def __init__(self, num, x, y):
+        self.num = num
+        self.x = x
+        self.y = y
+        self.width = 15
+        self.height = 30
+
+    def return_rectangle(self):
+        x1 = self.x-self.width/2
+        y1 = self.y
+        x2 = self.x+self.width/2
+        y2 = self.y-self.height
+        return (x1,y1,x2,y2)
+
+    def return_info(self):
+        info = self.return_rectangle()
+        data = "canon:{},{},{},{} ".format(info[1], info[2], info[3], info[4])
+        return data
+
+class Boulet:
+    def __init__(self, x, y):
+        self.rayon = 30
+        self.x = x
+        self.y = y
+        self.vx = 0
+        self.vy = 0
+
+    def return_rectangle(self):
+        #Servira pour la hitbox
+        x1 = self.x-self.rayon/2
+        y1 = self.y-self.rayon/2
+        x2 = self.x+self.rayon/2
+        y2 = self.y+self.rayon/2
+        return (x1,y1,x2,y2)
+
+    def deplacement(self):
+        self.x += vx
+        self.y += vy
+        self.vy -= 5
+
+    def return_info(self):
+        data = "boulet:{},{},{},{} ".format(self.x, self.y, self.vx, self.vy)
+        return data
+
+class Sol:
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+
+    def return_info(self):
+        data = "sol:{},{},{},{} ".format(self.x1, self.y1, self.x2, self.y2)
+        return data
+
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.bind(('', 12058))
@@ -106,3 +245,5 @@ game_list = []
 
 waiter = Waiter(socket, client_connected, adress_connected, listener_list, game_list)
 waiter.start()
+game_cleaner = Game_Cleaner(game_list)
+game_cleaner.start()
